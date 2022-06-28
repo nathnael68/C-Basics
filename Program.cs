1e3d518e2f311d6644dcb628f08ec11468af8844
nathnael68 <81167217+nathnael68@users.noreply.github.com>
2022-06-28 21:44:33 +0300

    Assignment 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Lab1;
+namespace program;
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter a number: ");
+        string i = Console.ReadLine();
+        int ia = Convert.ToInt32(i);
+        if (ia % 2 == 0)
+            Console.WriteLine("The value is even.");
+        else
+            Console.WriteLine("The value is odd.");
+
+        Student s = new Student();
+        s.id = 3;
+        s.name = "Nati";
+    }
+}

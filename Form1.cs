be0b15f0d6aa895d0003f14ff9f2648ec0b56d12
nathnael68 <81167217+nathnael68@users.noreply.github.com>
2022-07-17 00:26:10 +0300

    Updated assignment

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Product m1 = new Product
+            {
+                number = int.Parse(textBox1.Text),
+                date = datetxt.Text,
+                invnumb = int.Parse(textBox3.Text),
+                objname = textBox4.Text,
+                count = int.Parse(textBox5.Text),
+                price = double.Parse(textBox6.Text),
+            };
+            MessageBox.Show("SAVED");
+            m1.save();
+            ProductView1.DataSource = null;
+            ProductView1.DataSource = Product.GetAll();
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        
+
+        private void datetxt_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    internal class Product
+    {
+        static List<Product> products = new List<Product>();
+        public int number { get; set; }
+        public string date { get; set; }
+        public int invnumb { get; set; }
+        public string objname { get; set; }
+        public int count { get; set; }
+        public double price { get; set; }
+     
+        private static List<Product> product = new List<Product>();
+        public void save() { products.Add(this); }
+
+        public static List<Product> GetAll()
+        {
+            return products;
+        }
+       
+            
+    }
+}
